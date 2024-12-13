52dc0ae8085a7584be51dcd3528930ffa87822c9
LiDevMon <dbelonogov61@mail.ru>
2024-12-13 11:13:18 +0300

    Add OOP architecture, languages json file, select language logic

--- main.py
+++ main.py
@@ -1,51 +1,63 @@
-import dearpygui.dearpygui as dpg  #Import UI library
-from numpy.core.multiarray import item
+import json
 
+import dearpygui.dearpygui as dpg  #Import UI library
 from translator_selecter import *
 
-# Define variables
-original_language = "English"
-translate_language = "Russian"
+class App:
+    def __init__(self):
+        # Define variables
+        self.original_language = "English"
+        self.translate_language = "Russian"
 
-# Create Viewport for draw other windows
-dpg.create_context()
-dpg.create_viewport(title="MineTranslator", width=655, height=480)
-dpg.setup_dearpygui()
-# Callback for edit font size
-def get_font_size_callback(sender, data):
-    ...
+        # Languages and their codes
+        language_file = open('languages.json', 'rb')
+        self.languages = json.load(language_file)
+        self.lang_select = [x for x in self.languages.keys()]
 
-def select_translator(sender, data):
-    if(check_translator(data) == 0):
-        print("ERROR!")
-    elif(check_translator(data) == 1):
-        print('USE!')
-    elif(check_translator(data) == 2):
-        print("API!")
+    # Callback for edit font size
+    def get_font_size_callback(self, sender, data):
+        ...
 
-# Create main app window
-with dpg.window(tag="Main"):
-    # Menu bar
-    with dpg.viewport_menu_bar():
-        with dpg.menu(label="Settings"):
-            dpg.add_slider_int(label='Font scale', min_value=1, max_value=32, callback=get_font_size_callback)
-        with dpg.menu(label="Translator"): # Select translator base
-            dpg.add_combo(label="Select Translator", items=("Google", "Yandex", "DeepL"), callback=select_translator)
+    def select_translator(self, sender, data):
+        if(check_translator(data) == 0):
+            print("ERROR!")
+        elif(check_translator(data) == 1):
+            print('USE!')
+        elif(check_translator(data) == 2):
+            print("API!")
 
-    # Window with standard translator
-    # with dpg.child_window(label='Translator', pos=(0,10), width=640, height=400):
-    dpg.add_text("Translator")
-    with dpg.table(header_row=False) as table:
-        dpg.add_table_column(label="Original Language")
-        dpg.add_table_column(label="Translate Language")
-        with dpg.table_row():
-            for i in range(2):
-                dpg.add_combo(label="Choose language", default_value="English")
-        with dpg.table_row():
-            for i in range(2):
-                dpg.add_input_text(multiline=True, tab_input=True, height=300)
+    def run(self):
+        # Create Viewport for draw other windows
+        dpg.create_context()
+        dpg.create_viewport(title="MineTranslator", width=655, height=480)
+        dpg.setup_dearpygui()
+        # Create main app window
+        with dpg.window(tag="Main"):
+            # Menu bar
+            with dpg.menu_bar():
+                with dpg.menu(label="Settings"):
+                    dpg.add_slider_int(label='Font scale', min_value=1, max_value=32, callback=self.get_font_size_callback)
+                with dpg.menu(label="Translator"): # Select translator base
+                    dpg.add_combo(label="Select Translator", items=("Google", "Yandex", "DeepL"), callback=self.select_translator)
 
-dpg.show_viewport()
-dpg.set_primary_window('Main', True)
-dpg.start_dearpygui()
-dpg.destroy_context()
+            # Window with standard translator
+            # with dpg.child_window(label='Translator', pos=(0,10), width=640, height=400):
+            dpg.add_text("Translator")
+            with dpg.table(header_row=False) as table:
+                dpg.add_table_column(label="Original Language")
+                dpg.add_table_column(label="Translate Language")
+                with dpg.table_row():
+                    for i in range(2):
+                        dpg.add_combo(default_value="English", items=self.lang_select, callback=lambda x,y: print(self.languages[y]))
+                with dpg.table_row():
+                    for i in range(2):
+                        dpg.add_input_text(multiline=True, tab_input=True, height=300, width=300)
+
+        dpg.show_viewport()
+        dpg.set_primary_window('Main', True)
+        dpg.start_dearpygui()
+        dpg.destroy_context()
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
